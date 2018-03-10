fd484024c9cf874ded1372c9f6c64149f50842fe
Marcos De Moya <marcosdemoyalpz@gmail.com>
2018-03-10 03:14:51 -0400

    Fixed Date Intervals

--- app/src/main/java/com/mdemoya/practicas_altice/proyecto_curso_basico/AgeCalculatorActivity.java
+++ app/src/main/java/com/mdemoya/practicas_altice/proyecto_curso_basico/AgeCalculatorActivity.java
@@ -1,13 +1,122 @@
 package com.mdemoya.practicas_altice.proyecto_curso_basico;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
-public class AgeCalculatorActivity extends AppCompatActivity {
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.temporal.ChronoUnit;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
+public class AgeCalculatorActivity extends AppCompatActivity implements View.OnClickListener,
+        DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener {
+
+    Button datePickerNacimientoFromButton;
+
+    public Calendar dateFrom = Calendar.getInstance();
+    public SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MMMM/yyyy", Locale.US);
+    public SimpleDateFormat timeFormat = new SimpleDateFormat("hh:mm a", Locale.US);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_age_calculator);
+
+        datePickerNacimientoFromButton = findViewById(R.id.datePickerNacimientoButton);
+
+        findViewById(R.id.calcular_edad_button).setOnClickListener(this);
+
+        dateFrom = Calendar.getInstance();
+
+        datePickerNacimientoFromButton.setOnClickListener(v -> {
+            DatePickerDialog datePickerDialog =
+                    new DatePickerDialog(
+                            this,
+                            this,
+                            dateFrom.get(Calendar.YEAR),
+                            dateFrom.get(Calendar.MONTH),
+                            dateFrom.get(Calendar.DAY_OF_MONTH));
+            datePickerDialog.show();
+        });
+
+        Calendar nullTime = Calendar.getInstance();
+        nullTime.set(Calendar.HOUR_OF_DAY, 0);
+        nullTime.set(Calendar.MINUTE, 0);
+
+
+        datePickerNacimientoFromButton.setText(dateFormat.format(dateFrom.getTime()));
+        CalcularNacimiento(dateFrom);
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+        calendar.set(Calendar.MONTH, month);
+        calendar.set(Calendar.YEAR, year);
+
+        dateFrom = calendar;
+        datePickerNacimientoFromButton.setText(dateFormat.format(calendar.getTime()));
+    }
+
+    @Override
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+
+    }
+
+    private void CalcularNacimiento(Calendar nacimiento) {
+        Calendar today = Calendar.getInstance();
+
+        int currentYear = today.get(Calendar.YEAR);
+        int nacimientoYear = nacimiento.get(Calendar.YEAR);
+
+        int edad = currentYear - nacimientoYear;
+
+        // if dob is month or day is behind today's month or day
+        // reduce age by 1
+        int curMonth = today.get(Calendar.MONTH);
+        int dobMonth = nacimiento.get(Calendar.MONTH);
+        if (dobMonth > curMonth) { // this year can't be counted!
+            edad--;
+        } else if (dobMonth == curMonth) { // same month? check for day
+            int curDay = today.get(Calendar.DAY_OF_MONTH);
+            int dobDay = nacimiento.get(Calendar.DAY_OF_MONTH);
+            if (dobDay > curDay) { // this year can't be counted!
+                edad--;
+            }
+        }
+
+        TextView resultText = findViewById(R.id.result_text_nacimiento);
+        String result = getString(R.string.defaultResultTime);
+
+        result = edad + " " + getString(R.string.yearsSuffix);
+
+        resultText.setText(result);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.calcular_edad_button:
+                CalcularNacimiento(dateFrom);
+                break;
+
+            default:
+                Toast.makeText(this, "Unknown button clicked!", Toast.LENGTH_LONG).show();
+                break;
+        }
     }
 }
--- app/src/main/java/com/mdemoya/practicas_altice/proyecto_curso_basico/DateIntervalActivity.java
+++ app/src/main/java/com/mdemoya/practicas_altice/proyecto_curso_basico/DateIntervalActivity.java
@@ -4,47 +4,96 @@ import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.TextView;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Locale;
 
-public class DateIntervalActivity extends AppCompatActivity
-        implements DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener {
+public class DateIntervalActivity extends AppCompatActivity implements View.OnClickListener,
+        DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener {
 
-    Button datePickerButton;
-    Button timePickerButton;
+    Button datePickerIntervalFromButton, timePickerIntervalFromButton,
+            datePickerIntervalToButton, timePickerIntervalToButton;
+
+    int hoursFrom, hoursTo, minutesFrom, minutesTo = 0;
+    boolean isDateFrom = true;
+    public Calendar currentDate, dateFrom, dateTo = Calendar.getInstance();
+    public SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MMMM/yyyy", Locale.US);
+    public SimpleDateFormat timeFormat = new SimpleDateFormat("hh:mm a", Locale.US);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_date_interval);
 
-        datePickerButton = findViewById(R.id.datePickerIntervalFromButton);
-        timePickerButton = findViewById(R.id.timePickerIntervalFromButton);
+        datePickerIntervalFromButton = findViewById(R.id.datePickerIntervalFromButton);
+        timePickerIntervalFromButton = findViewById(R.id.timePickerIntervalFromButton);
+        datePickerIntervalToButton = findViewById(R.id.datePickerIntervalToButton);
+        timePickerIntervalToButton = findViewById(R.id.timePickerIntervalToButton);
 
-        datePickerButton.setOnClickListener(v -> {
+        findViewById(R.id.calcular_intervalo_button).setOnClickListener(this);
+
+        hoursFrom = hoursTo = 0;
+        dateTo = dateFrom = currentDate = Calendar.getInstance();
+
+        datePickerIntervalFromButton.setOnClickListener(v -> {
             DatePickerDialog datePickerDialog =
                     new DatePickerDialog(
                             this,
                             this,
-                            2018,
-                            02,
-                            1);
+                            currentDate.get(Calendar.YEAR),
+                            currentDate.get(Calendar.MONTH),
+                            currentDate.get(Calendar.DAY_OF_MONTH));
+            isDateFrom = true;
             datePickerDialog.show();
         });
 
-        timePickerButton.setOnClickListener(v -> {
+        datePickerIntervalToButton.setOnClickListener(v -> {
+            DatePickerDialog datePickerDialog =
+                    new DatePickerDialog(
+                            this,
+                            this,
+                            currentDate.get(Calendar.YEAR),
+                            currentDate.get(Calendar.MONTH),
+                            currentDate.get(Calendar.DAY_OF_MONTH));
+            isDateFrom = false;
+            datePickerDialog.show();
+        });
+
+        Calendar nullTime = Calendar.getInstance();
+        nullTime.set(Calendar.HOUR_OF_DAY, 0);
+        nullTime.set(Calendar.MINUTE, 0);
+
+        timePickerIntervalFromButton.setOnClickListener(v -> {
             TimePickerDialog timePickerDialog = new TimePickerDialog(this,
                     this,
-                    3,
-                    59,
+                    0,
+                    0,
                     false);
+            isDateFrom = true;
             timePickerDialog.show();
         });
+        timePickerIntervalToButton.setOnClickListener(v -> {
+            TimePickerDialog timePickerDialog = new TimePickerDialog(this,
+                    this,
+                    0,
+                    0,
+                    false);
+            isDateFrom = false;
+            timePickerDialog.show();
+        });
+
+        datePickerIntervalFromButton.setText(dateFormat.format(currentDate.getTime()));
+        datePickerIntervalToButton.setText(dateFormat.format(currentDate.getTime()));
+        timePickerIntervalFromButton.setText(timeFormat.format(nullTime.getTime()));
+        timePickerIntervalToButton.setText(timeFormat.format(nullTime.getTime()));
+        CalcularIntervalo(findViewById(R.id.calcular_intervalo_button));
     }
 
     @Override
@@ -54,9 +103,13 @@ public class DateIntervalActivity extends AppCompatActivity
         calendar.set(Calendar.MONTH, month);
         calendar.set(Calendar.YEAR, year);
 
-        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MMMM/yyyy",
-                Locale.US);
-        datePickerButton.setText(dateFormat.format(calendar.getTime()));
+        if (isDateFrom == true) {
+            dateFrom = calendar;
+            datePickerIntervalFromButton.setText(dateFormat.format(calendar.getTime()));
+        } else {
+            dateTo = calendar;
+            datePickerIntervalToButton.setText(dateFormat.format(calendar.getTime()));
+        }
     }
 
     @Override
@@ -65,8 +118,55 @@ public class DateIntervalActivity extends AppCompatActivity
         calendar.set(Calendar.HOUR_OF_DAY, hourOfDay);
         calendar.set(Calendar.MINUTE, minute);
 
-        SimpleDateFormat dateFormat = new SimpleDateFormat("hh:mm a",
-                Locale.US);
-        timePickerButton.setText(dateFormat.format(calendar.getTime()));
+        if (isDateFrom == true) {
+            hoursFrom = hourOfDay;
+            minutesFrom = minute;
+            timePickerIntervalFromButton.setText(timeFormat.format(calendar.getTime()));
+        } else {
+            hoursTo = hourOfDay;
+            minutesTo = minute;
+            timePickerIntervalToButton.setText(timeFormat.format(calendar.getTime()));
+        }
+    }
+
+    private void CalcularIntervalo(View view) {
+        switch (view.getId()) {
+            case R.id.calcular_intervalo_button:
+                long seconds = ((dateTo.getTimeInMillis()) - (dateFrom.getTimeInMillis())) / 1000;
+                long hours = (seconds / 3600);
+                hours = hours + (hoursTo - hoursFrom);
+                seconds = (hours * 3600) + ((minutesTo - minutesFrom) * 60);
+                long millisecs = seconds * 1000;
+
+                seconds = millisecs / 1000 % 60;
+                long minutes = millisecs / (60 * 1000) % 60;
+                hours = millisecs / (60 * 60 * 1000) % 24;
+                long days = millisecs / (24 * 60 * 60 * 1000);
+
+                TextView resultText = findViewById(R.id.result_text_interval);
+                String result = getString(R.string.defaultResultTime);
+
+//                result = years + " " + getString(R.string.yearsSuffix)
+//                        + " " + months + " " + getString(R.string.monthsSuffix)
+//                        + "\n" + days + " " + getString(R.string.daysSuffix)
+//                        + " " + hours + " " + getString(R.string.hoursSuffix);
+
+                result = days + " " + getString(R.string.daysSuffix)
+                        + ", " + hours + " " + getString(R.string.hoursSuffix)
+                        + ",\n" + minutes + " " + getString(R.string.minutesSuffix);
+//                        + ", " + seconds + " " + getString(R.string.secondsSuffix);
+
+                resultText.setText(result);
+                break;
+
+            default:
+                Toast.makeText(this, "Unknown button clicked!", Toast.LENGTH_LONG).show();
+                break;
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        CalcularIntervalo(view);
     }
 }
